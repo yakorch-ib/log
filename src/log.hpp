383f6e34dc5d4b0161c3b16e429c96a4a34bb310
yakorch-ib <yaroslav.k@ironbelly.systems>
2026-04-14 16:19:09 +0300

    change yellow to a color w/ less contrast.

--- src/log.hpp
+++ src/log.hpp
@@ -128,7 +128,7 @@ void log_impl(log_level_t level, int line, std::string_view file_name,
       case log_level_t::info:
         return fmt::fg(lighter(fmt::color::light_gray, -0.5));
       case log_level_t::warning:
-        return fmt::bg(fmt::color::yellow) | fmt::fg(fmt::color::black);
+        return fmt::fg(fmt::color::golden_rod);
       case log_level_t::error:
         return fmt::bg(fmt::color::indian_red) | fmt::fg(fmt::color::white);
     }
